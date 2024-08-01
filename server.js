8e118d89b4f6738c627df84f1efe79c183be42b2
duncmv <dtindiwensi@gmail.com>
2024-08-01 15:49:19 +0300

    task 2 & 3

--- server.js
+++ server.js
@@ -6,4 +6,4 @@ const PORT = process.env.PORT || 5000;
 const app = express();
 app.use(express.json());
 mapRoutes(app);
-app.listen(PORT, () => console.log(`Server started on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));

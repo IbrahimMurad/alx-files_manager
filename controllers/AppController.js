96ec39ac6064434a3eaa30bac56c8b22a666cdbb
IbrahimMurad <ibrahimmorad31@gmail.com>
2024-08-01 16:38:56 +0300

    Edits: send => json , await nbusers and nbFiles, return after error to end the middleware from continueing (it is not flask sadly).

--- controllers/AppController.js
+++ controllers/AppController.js
@@ -1,14 +1,18 @@
-import dbClient from "../utils/db";
-import redisClient from "../utils/redis";
+import dbClient from '../utils/db';
+import redisClient from '../utils/redis';
 
 export default class AppController {
   static getStatus(req, res) {
     if (redisClient.isAlive() && dbClient.isAlive()) {
-      res.status(200).send({ redis: true, db: true });
+      res.status(200).json({ redis: true, db: true });
     }
   }
 
-  static getStats(req, res) {
-    res.status(200).send({ users: dbClient.nbUsers(), files: dbClient.nbFiles() });
+  static async getStats(req, res) {
+    const stats = {
+      users: await dbClient.nbUsers(),
+      files: await dbClient.nbFiles(),
+    };
+    res.status(200).json(stats);
   }
 }
